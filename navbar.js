48feb0dd125baaebe909506cf664702a53f2ccf2
upendra2065 <upendrakumariitian2017@gmail.com>
2018-12-08 23:49:04 +0530

    Blog frontend commit

--- navbar.js
+++ navbar.js
@@ -1,5 +1,13 @@
-$(function(){
-    $("#login").click(function () {  
-        $("#form1").toggle();
-    });
-});
+
+$("#suscribe").on("click", function(){
+    $(".popup-overlay, .popup-content").addClass("active");
+    $("#pop-up-background").addClass("active"); 
+    //$("#body").hide();
+  });
+  
+  //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+  $("#close,.close").on("click", function(){
+    $(".popup-overlay, .popup-content").removeClass("active");
+    $("#pop-up-background").removeClass("active"); 
+  }); 
+  
