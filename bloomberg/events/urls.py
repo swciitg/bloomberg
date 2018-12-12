bb14825670a4d364884aff973c925904c2481cf7
aman <ar20603@gmail.com>
2018-12-12 22:23:20 +0530

    admindash views added

--- bloomberg/events/urls.py
+++ bloomberg/events/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('upload/', views.eventUpload, name='eventUpload'),
     path('eventlive/<int:pk>', views.eventlive, name= 'eventlive'),
     path('eventblock/<int:pk>', views.eventblock, name= 'eventblock'),
+    path('admin/',views.admindashevents, name= 'admindashevents'),
+    path('admin/pending/',views.pendingevent, name= 'pendingevent'),
+    path('admin/new/',views.newevent, name= 'newevent'),
 ]
--- bloomberg/events/views.py
+++ bloomberg/events/views.py
@@ -1,37 +1,40 @@
 import datetime
-from ../blogs.models import UserDetail
+from blogs.models import UserDetail
 from .forms import EventUploadForm
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Event
 
-
-def event (request,pk):
-    event = get_object_or_404(Event , pk=pk)
-    context = {
-        'event' : event ,
-    }
-
-    return render(request, ,context)
+#
+# def event (request,pk):
+#     event = get_object_or_404(Event , pk=pk)
+#     context = {
+#         'event' : event ,
+#     }
+#
+#     return render(request, ,context)
 
 def eventUpload (request):
     if request.method == 'POST':
         if request.session.has_key('eid'):
+            emailID = request.session['eid']
+            user = UserDetail.objects.get(emailID = emailID)
             event_form = EventUploadForm(request.POST , request.FILES)
             title=request.POST['title']
             image=request.FILES['image']
             venue=request.POST['venue']
-            associatedClub=request.POST['associatedClub']
+            organizingClub=request.POST['associatedClub']
             description=request.POST['description']
             date=request.POST['date']
 
             if event_form.is_valid():
                 Event.objects.create(
+                    postedBy=user.name,
                     title=title,
                     image=image,
                     venue=venue,
-                    associatedClub=associatedClub,
+                    organizingClub=organizingClub,
                     description=description,
                     date=date,
                 )
@@ -44,19 +47,20 @@ def eventUpload (request):
         event_form=EventUploadForm()
 
     context = {
-        'form' = event_form,
+        'form' : event_form,
     }
 
-    return render(request, ,context)
+    return render(request, 'events/eventupload.html' ,context)
 
 def eventlive(request,pk):
     event=get_object_or_404(Event, pk=pk)
 
-    if request.session.has_key()'eid'):
+    if request.session.has_key('eid'):
         emailID=request.session['eid']
         user=UserDetail.objects.get(emailID__exact=emailID)
         if user.isAdmin:
             event.isLive = True
+            event.approvedBy=user.name
             event.save()
             return HttpResponseRedirect(reverse('blogs:pendingpost'))
         else:
@@ -67,14 +71,73 @@ def eventlive(request,pk):
 def eventblock(request,pk):
     event=get_object_or_404(Event, pk=pk)
 
-    if request.session.has_key()'eid'):
+    if request.session.has_key('eid'):
         emailID=request.session['eid']
         user=UserDetail.objects.get(emailID__exact=emailID)
         if user.isAdmin:
             event.isLive = False
+            event.approvedBy=user.name
             event.save()
             return HttpResponseRedirect(reverse('blogs:pendingpost'))
         else:
             return HttpResponseRedirect(reverse('blogs:permissiondenied'))
     else:
         return HttpResponseRedirect(reverse('blogs:login'))
+
+def admindashevents (request):
+	if request.session.has_key('eid'):
+		emailID = request.session['eid']
+		user = UserDetail.objects.get(emailID = emailID)
+		if not user.isAdmin:
+			return HttpResponseRedirect(reverse('blogs:permissiondenied'))
+		if user.isBlocked:
+			return HttpResponseRedirect(reverse('blogs:permissiondenied'))
+		events = Event.objects.all()
+		page_title = 'ALL EVENTS'
+		context = {
+			'user' : user ,
+			'events' : events ,
+			'page_title' : page_title,
+		}
+
+		return render(request , 'events/admindashevents.html' , context)
+
+	return HttpResponseRedirect(reverse('blogs:login'))
+
+def pendingevent(request):
+	if request.session.has_key('eid'):
+		emailID = request.session['eid']
+		user = UserDetail.objects.get(emailID = emailID)
+		if not user.isAdmin:
+			return HttpResponseRedirect(reverse('blogs:permissiondenied'))
+		if user.isBlocked:
+			return HttpResponseRedirect(reverse('blogs:permissiondenied'))
+		events = Event.objects.filter(approvedBy=None)
+
+		page_title='PENDING EVENTS'
+		context = {
+			'user' : user ,
+			'events' : events ,
+			'page_title' : page_title,
+		}
+		return render(request , 'blogs/admindashevents.html' , context)
+
+def newevent(request):
+	if request.session.has_key('eid'):
+		emailID = request.session['eid']
+		user = UserDetail.objects.get(emailID = emailID)
+		if not user.isAdmin:
+			return HttpResponseRedirect(reverse('blogs:permissiondenied'))
+		if user.isBlocked:
+			return HttpResponseRedirect(reverse('blogs:permissiondenied'))
+		page_title='NEW EVENTS'
+		events = Event.objects.all().order_by('-date')
+		context = {
+			'user' : user ,
+			'events' : events ,
+			'page_title' : page_title,
+		}
+
+		return render(request , 'blogs/admindashevents.html' , context)
+
+	return HttpResponseRedirect(reverse('blogs:login'))
