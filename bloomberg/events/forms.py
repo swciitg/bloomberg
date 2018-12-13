ae66489ab55399394b9463599ce7ae7f6b270a0b
Anubhav <tyagianubhav619@gmail.com>
2018-12-13 13:51:23 +0530

    forms.py fixed

--- bloomberg/events/forms.py
+++ bloomberg/events/forms.py
@@ -1,14 +1,23 @@
 import datetime
 from django import forms
-from .models import UserDetail
+from .models import Event
+from django_unixdatetimefield import UnixDateTimeField
+from blogs.models import UserDetail
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 
-class EventUploadForm(forms.Form):
-    title = forms.CharField(max_length = 128)
-    image = forms.ImageField()
-    venue = forms.CharField(max_length = 128)
-    associatedClub = forms.CharField(max_length = 128)
-    description = forms.CharField(max_length = 500)
-    date = forms.DateField()
+class EventUploadForm(forms.ModelForm):
+    # title = forms.CharField(max_length = 128)
+    # image = forms.ImageField()
+    # venue = forms.CharField(max_length = 128)
+    # associatedClub = forms.CharField(max_length = 128)
+    # description = forms.CharField(max_length = 500)
+    # date = forms.DateField(widget=forms.widgets.DateInput(format="%d/%m/%Y"))
+    #
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #
+    #     return cleaned_data
+    class Meta:
+        model = Event
