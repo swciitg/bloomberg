764c10941b5b66ea2056fa1f5ad644d0665e0501
Anubhav <tyagianubhav619@gmail.com>
2018-12-30 13:36:14 +0530

    a

--- bloomberg/polls/views.py
+++ bloomberg/polls/views.py
@@ -1,6 +1,7 @@
 from .models import Question, Choice
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F
 from django.http import HttpResponseRedirect
 
 # Create your views here.
@@ -13,7 +14,7 @@ def vote(request, question_id):
 
         return HttpResponseRedirect(reverse('main:index'))
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('main:index'))
